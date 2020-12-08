46ed6131edbd96d85f776d7654417ccce28e261d
배규덕 <mandarinduk@gmail.com>
2020-12-08 15:33:12 +0900

    [JS 100제] - 51~52번 완료 - 나중에 다시 해보기!!

---
+++ JS100제/20201208.js
@@ -0,0 +1,66 @@
+/**
+ * [JS100제] 문제 51번
+ * =================================================================
+ * 병합정렬(merge sort)은 대표적인 정렬 알고리즘 중 하나로 다음과 같이 동작합니다.
+ * 1. 리스트의 길이가 0 또는 1이면 이미 정렬된 것으로 본다.
+ * 2. 그렇지 않은 경우에는 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눈다.
+ * 3. 각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬한다.
+ * 4. 두 부분 리스트를 다시 하나의 정렬된 리스트로 합병한다.
+ * =================================================================
+ */
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  while (left.length) {
+    result.push(left.shift());
+  }
+  while (right.length) {
+    result.push(right.shift());
+  }
+
+  return result;
+}
+
+/**
+ * [JS100제] 문제 52번
+ * =================================================================
+ * 다음 빈 칸을 채워 퀵 정렬을 완성해주세요.
+ * =================================================================
+ */
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+}
